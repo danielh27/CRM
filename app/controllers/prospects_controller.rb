ac343671bc3276558b07d2dcc456c3b843cf4bec
Daniel Herrera <daniel97hh@gmail.com>
2022-02-26 17:51:23 -0500

    se crean consultas para visualizar la cantidad de prospectos por status en el index

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -1,4 +1,7 @@
 class ProspectsController < ApplicationController
   def index
+    @prospects_quantity = Prospect.where(status: 'prospect').count
+    @interested_quantity = Prospect.where(status: 'interested').count
+    @clients_quantity = Prospect.where(status: 'client').count
   end
 end
