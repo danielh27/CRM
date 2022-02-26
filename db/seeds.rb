bd5bf15354eafa419d7a2b4fb60720a4afe2fb3c
Daniel Herrera <daniel97hh@gmail.com>
2022-02-26 16:43:43 -0500

    seeds para crear 3 usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.find_or_create_by(email: 'admin@gmail.com', password: '291ABASD932' )
+User.find_or_create_by(email: 'usuario1@gmail.com', password: '291ABASD934' )
+User.find_or_create_by(email: 'usuario2@gmail.com', password: '281ABASD932' )
