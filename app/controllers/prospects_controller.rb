b82058bc6b54c76320572ef44d8c97d4f0e4d033
Daniel Herrera <daniel97hh@gmail.com>
2022-02-26 18:18:11 -0500

    tabla para visualizar prospectos

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -3,5 +3,7 @@ class ProspectsController < ApplicationController
     @prospects_quantity = Prospect.where(status: 'prospect').count
     @interested_quantity = Prospect.where(status: 'interested').count
     @clients_quantity = Prospect.where(status: 'client').count
+
+    @list_prospects = Prospect.all
   end
 end
